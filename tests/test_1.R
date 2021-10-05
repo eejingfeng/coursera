d045ec64523287451b9d63224890fc8dae6d2216
Carl <eejingfeng@hotmail.com>
2021-10-05 19:34:03 +0800

    Update test_1.R

--- tests/test_1.R
+++ tests/test_1.R
@@ -1,20 +1,5 @@
-install.packages("testthat")
+testthat::test_that('check filename is created properly', {
+  filename_2013 <- make_filename(2013)
 
-library(testthat)
-library(fars)
-library(dplyr)
-library(readr)
-library(tidyr)
-library(magrittr)
-library(graphics)
-library(maps)
-
-# test make_filename
-test_that("Testing the make_filename function", {
-       expect_equal(make_filename(2013), "accident_2013.csv.bz2")})
-
-test_that("Testing the make_filename function", {
-       expect_equal(make_filename(2014), "accident_2014.csv.bz2")})
-
-test_that("Testing the make_filename function", {
-       expect_equal(make_filename(2015), "accident_2015.csv.bz2")})
+  expect_that(filename_2013, is_a('character'))
+})
